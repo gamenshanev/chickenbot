23900492528ee809a4a3686298e38837f49a1ce6
gamenshanev <49565972+gamenshanev@users.noreply.github.com>
2019-09-09 08:11:58 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -14,10 +14,11 @@ module.exports.run = async (bot, message, arguments) => {
     if(banUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Hey je mag een mede staff niet bannen!");
 
     var ban = new discord.RichEmbed()
-        .setDescription("ban")
-        .setColor("ee0000")
-        .addField("Geband door", message.author)
-        .addField("Reden", reason);
+    .setDescription("Ban")
+    .setColor("#03fcec")
+    .addField("Banned Gebruiker:", banUser)
+    .addField("Gebant Door", message.author)
+    .addField("Reden:", reason);
 
     var banChannel = message.guild.channels.find(`name`, "⚙logs⚙");
     if(!banChannel) return message.guild.send("Channel not found.");
